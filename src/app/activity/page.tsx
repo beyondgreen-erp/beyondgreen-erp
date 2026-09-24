09f2653840acdb06c6066e5cc905d93e08cb5d94
RudyPerrier <rperrier171991@gmail.com>
2026-09-23 18:01:13 -0700

    Add ActivityPage component with ActivityFeedFull

---
+++ src/app/activity/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+export const dynamic = 'force-dynamic'
+import ActivityFeedFull from '@/components/ActivityFeedFull'
+
+export default function ActivityPage() {
+  return <ActivityFeedFull />
+}
